905792c33e7ba6e0e27b992c649369ae7741fa05
Ronak Jangid <ronakjangid2211@gmail.com>
2023-07-27 19:27:11 +0530

    automation sucessfull

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import os
 import shutil
+import time
 
 path = "/home/itachidesktop/Downloads"
 photos_path = "/home/itachidesktop/Downloads/Photos"
 
-list_dir = os.listdir(path)
 
 
 def is_photo_file(file_path):
     _, extension = os.path.splitext(file_path)
-    if(extension.lower() == '.jpg' or extension.lower() == '.png'):
+    if(extension.lower() == '.jpg' or extension.lower() == '.png' or extension.lower() == '.jpeg'):
         shutil.move(file_path, photos_path)
         print('File Move Success')
 
     else: print('Unknown file')
 
 
-file_name = 'peakpx.jpg'
-full_file_path = os.path.join(path, file_name)
-is_photo_file(full_file_path)
+while True:
+    list_dir = os.listdir(path)
+
+    for file in list_dir:
+        full_file_path = os.path.join(path, file)
+        is_photo_file(full_file_path)
+
+    time.sleep(10)
